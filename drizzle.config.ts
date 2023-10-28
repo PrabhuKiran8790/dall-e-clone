ff64225d6c59e446a433f9533d7be37a04cb3da0
Prabhu Kiran Konda <prabhukiran8790@pm.me>
2023-10-28 11:23:44 +0530

    added node runtime

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,9 +1,10 @@
 import type { Config } from 'drizzle-kit';
-import * as dotenv from 'dotenv';
 
-dotenv.config({
-	path: '.env'
-});
+// import * as dotenv from 'dotenv';
+
+// dotenv.config({
+// 	path: '.env'
+// });
 
 export default {
 	schema: './src/lib/db/schema.ts',
