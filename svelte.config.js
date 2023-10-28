c70556e383fb1988b8e94a55b352d7bd759d4bf3
Prabhu Kiran Konda <prabhukiran8790@pm.me>
2023-10-28 11:03:32 +0530

    initial commit

---
+++ svelte.config.js
@@ -0,0 +1,25 @@
+import adapter from "@sveltejs/adapter-auto";
+import { vitePreprocess } from "@sveltejs/kit/vite";
+import { mdsvex } from "mdsvex";
+
+/** @type {import('mdsvex').MdsvexOptions} */
+const mdsvexOptions = {
+	extensions: [".md", ".svx"],
+	layout: {
+		_: "src/routes/terms-of-service/__layout.svelte",
+	},
+};
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
+	// for more information about preprocessors
+	extensions: [".svelte", ...mdsvexOptions.extensions],
+	preprocess: [vitePreprocess({}), mdsvex(mdsvexOptions)],
+
+	kit: {
+		adapter: adapter(),
+	},
+};
+
+export default config;
